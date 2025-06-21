eb7b33008393b021b195d9bfddc3c76f33c237f0
Shahdhelaly19 <shahdhelaly82@gmail.com>
2025-06-21 04:55:58 +0300

    🔧 Replace Firebase JSON with env-based setup

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,14 +1,28 @@
-import fs from 'fs/promises';
+// import fs from 'fs/promises';
+// import admin from 'firebase-admin';
+
+// // Initialization once
+// const serviceAccount = JSON.parse(
+//   await fs.readFile("./firebase/firebaseServiceAccountKey.json", "utf-8")
+// );
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
+
+// export default admin;
 import admin from 'firebase-admin';
 
-// Initialization once
-const serviceAccount = JSON.parse(
-  await fs.readFile("./firebase/firebaseServiceAccountKey.json", "utf-8")
-);
-
+// Initializing Firebase Admin with ENV variables
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
   });
 }
 
