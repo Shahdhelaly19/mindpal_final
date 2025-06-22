898af5ac4a7984e65d6ff50a78f45e8b8b5e647f
Shahdhelaly19 <shahdhelaly82@gmail.com>
2025-06-22 19:24:11 +0300

    edit sendNotification

--- src/utils/sendNotification.js
+++ src/utils/sendNotification.js
@@ -1,11 +1,10 @@
-import fs from 'fs/promises';
 import admin from 'firebase-admin';
 
-const serviceAccount = JSON.parse(
-  await fs.readFile("./firebase/firebaseServiceAccountKey.json", "utf-8")
-);
-
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_KEY_BASE64, "base64").toString("utf8")
+  );
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
